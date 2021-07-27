ac848e4f89d62f43a737acbaf89329fd6e6d2c28
Dallas Pataska <pataskad@gmail.com>
2021-07-26 21:29:20 -0500

    'hover' ability added via mouseover javascript event listener, reset button initialized but only working with first div so far

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 'use strict';
 
-// container node housing div grid
+
 const container = document.querySelector('.container');
-
-// create node div elements before named function to/and hold in memory? 
-// const divs = document.createElement('div');
-
-// container.appendChild(divs);
 
 let squares = 144;
 
-
+// core program to create divs within container
 for (let i = 0; i < squares; i++) {
     const divs = document.createElement('div');
     divs.className = 'js-div';
-
     container.appendChild(divs);
+    divs.addEventListener('mouseover', colorChange); // add event listener to every created div
 }
+
+function colorChange(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+const newDivs = document.querySelector('.js-div'); // only sets first div
+
+
+const resetBtn = document.querySelector('#grid-reset');
+resetBtn.addEventListener('click', () => {
+    newDivs.style.backgroundColor = '';
+});
+
