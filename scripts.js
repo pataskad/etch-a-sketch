0137a400c8f1bd6f83bb7816fa0194b5a8880ef6
Dallas Pataska <pataskad@gmail.com>
2021-08-02 13:55:29 -0500

    resetBackground refactor, reducing code by 50% for same impact

--- scripts.js
+++ scripts.js
@@ -3,11 +3,12 @@
 const container = document.querySelector('.container');
 const resetBtn = document.querySelector('#grid-reset');
 
-let squareSideCount = 32; // limit squares to 100
+let squareSideCount = 16; // limit squares to 100
 let divs = [];
 
 createDivGrid(squareSideCount);
-// core program to create divs within container, 16x16 grid
+
+// core program to create divs within container, 16x16 grid baseline
 function createDivGrid(squareSideCount) {
     container.style.gridTemplateColumns = (`repeat(${squareSideCount}, 1fr`);
     container.style.gridTemplateRows = (`repeat(${squareSideCount}, 1fr`);
@@ -16,24 +17,16 @@ function createDivGrid(squareSideCount) {
     for (let y = 0; y < cellCount; y++) {
         divs[y] = document.createElement('div');
         divs[y].classList.add('js-div');
-        //divs[y].className = 'js-div-regular';
         divs[y].addEventListener('mouseover', colorChange);
         container.appendChild(divs[y]);
     }
 }
-
 function colorChange(e) {
     e.target.style.backgroundColor = 'black';
 }
-
-const newDivs = document.querySelectorAll('.js-div'); // only sets first div
-// grab nodeList
-// loop through nodeList upon function call 'button click'
-// reset background color of each div within the container
 function resetBackground() {
-    for (let j = 0; j < newDivs.length; j++) {
-        let item = newDivs[j];
-        item.style.backgroundColor = '';
+    for (let j = 0; j < divs.length; j++) {
+        divs[j].style.backgroundColor = '';
     }
 }
 function setSquares() {
