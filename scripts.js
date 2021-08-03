210611759788d43be42015d217ada63a8ad7a5d4
Dallas Pataska <pataskad@gmail.com>
2021-08-03 14:25:33 -0500

    Implemented working user input functionality to allow dynamic grid square counts, next step is to set up parameters/input restrictions

--- scripts.js
+++ scripts.js
@@ -3,18 +3,20 @@
 const container = document.querySelector('.container');
 const resetBtn = document.querySelector('#grid-reset');
 const main = document.querySelector('#main-content');
+const formBtn = document.getElementById('input-submit-btn');
+const formInput = document.getElementById('input-box');
 
 let squareSideCount = 16; // limit squares to 100
 let divs = [];
 
-createDivGrid(squareSideCount);
+createDivGrid(squareSideCount); // initialize starting grid count
 
 // core program to create divs within container, 16x16 grid baseline
 function createDivGrid(squareSideCount) {
+    clearGrid();
     container.style.gridTemplateColumns = (`repeat(${squareSideCount}, 1fr`);
     container.style.gridTemplateRows = (`repeat(${squareSideCount}, 1fr`);
     let cellCount = squareSideCount * squareSideCount;
-
     for (let y = 0; y < cellCount; y++) {
         divs[y] = document.createElement('div');
         divs[y].classList.add('js-div');
@@ -22,6 +24,8 @@ function createDivGrid(squareSideCount) {
         container.appendChild(divs[y]);
     }
 }
+
+// helper functions
 function colorChange(e) {
     e.target.style.backgroundColor = 'black';
 }
@@ -30,23 +34,39 @@ function resetBackground() {
         divs[j].style.backgroundColor = '';
     }
 }
+function removeBlur() {
+    main.classList.remove('blur');
+}
+function clearGrid() {
+    // removes all current children to allow for new grid count
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
 
+// event listeners
 resetBtn.addEventListener('click', () => {
     resetBackground();
     modal.style.display = "block";
     main.classList.add('blur');
+});
+formBtn.addEventListener('click', () => {
+    squareSideCount = formInput.value;
+    createDivGrid(squareSideCount);
+    modal.style.display = "none";
+    removeBlur();
 });
 
 // modal content and functions
 let modal = document.getElementById("input-modal");
 let span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
-    main.classList.remove('blur');
     modal.style.display = "none";
+    removeBlur();
 }
 window.onclick = function(e) {
     if (e.target == modal) {
-        main.classList.remove('blur');
         modal.style.display = "none";
+        removeBlur();
     }
 }
